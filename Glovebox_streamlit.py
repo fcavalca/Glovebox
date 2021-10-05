3f39bfc75b466882342cae8877ef90ce0ba97195
fcavalca <33277841+fcavalca@users.noreply.github.com>
2021-10-05 15:24:43 +0200

    Update Glovebox_streamlit.py

--- Glovebox_streamlit.py
+++ Glovebox_streamlit.py
@@ -21,7 +21,8 @@ if file:
     
     #Make pandas dataframe from raw data
     data = pd.read_csv(file, sep='\t', header=9, index_col=0)
-
+    data
+    len(data.columns)
     data.index =  pd.to_datetime(data.index, format="%d.%m.%Y %H:%M:%S")
 
     tmin = st.sidebar.date_input("Select start date", min_value=data.index.min(), max_value=data.index.max(), value=data.index.min())
@@ -67,24 +68,43 @@ if file:
     
     #%%
     #fig.add_trace(px.scatter(data, x=data.index, y=data[data.columns[0]]),row=1, col=1)
+    if len(data.columns)>5:
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[0]], name = data.columns[0]),row=1, col=1)
+        fig.update_yaxes(title_text="Partial pressures (ppm)", row=1, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[1]], name = data.columns[1]),row=1, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[2]], name = data.columns[2]),row=2, col=1)
+        fig.update_yaxes(title_text="Pressures", row=2, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[3]], name = data.columns[3]),row=2, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[4]], name = data.columns[4]),row=3, col=1)
+        fig.update_yaxes(title_text="Temperatures (°C)", row=3, col=1)
     
-    fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[0]], name = data.columns[0]),row=1, col=1)
-    fig.update_yaxes(title_text="Partial pressures", row=1, col=1)
     
-    fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[1]], name = data.columns[1]),row=1, col=1)
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[5]], name = data.columns[5]),row=3, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[6]], name = data.columns[6]),row=3, col=1)
+        fig.update_xaxes(title_text=data.index.name, row=3, col=1)
+        
+    else:
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[0]], name = data.columns[0]),row=1, col=1)
+        fig.update_yaxes(title_text="Partial pressures (ppm)", row=1, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[1]], name = data.columns[1]),row=1, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[2]], name = data.columns[2]),row=2, col=1)
+        fig.update_yaxes(title_text="Pressure", row=2, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[3]], name = data.columns[3]),row=3, col=1)
+        
+        fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[4]], name = data.columns[4]),row=3, col=1)
+        fig.update_yaxes(title_text="Temperatures (°C)", row=3, col=1)
     
-    fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[2]], name = data.columns[2]),row=2, col=1)
-    fig.update_yaxes(title_text="Pressures", row=2, col=1)
     
-    fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[3]], name = data.columns[3]),row=2, col=1)
+        
     
-    fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[4]], name = data.columns[4]),row=3, col=1)
-    fig.update_yaxes(title_text="Temperatures", row=3, col=1)
-    
-    fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[5]], name = data.columns[5]),row=3, col=1)
-    
-    fig.add_trace(go.Scattergl(x=data.index, y=data[data.columns[6]], name = data.columns[6]),row=3, col=1)
-    fig.update_xaxes(title_text=data.index.name, row=3, col=1)
     fig.update_xaxes(range=[tmin, tmax])
     fig.update_yaxes(autorange=True, fixedrange=False)
 
